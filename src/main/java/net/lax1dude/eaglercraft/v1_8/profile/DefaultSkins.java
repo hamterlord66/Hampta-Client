3d4312f89af2b104577bcf9a4d51d75e7edd9e5b
Joshua Rattles <363243@sps.org>
2026-08-17 07:31:52 -0500

    Update DefaultSkins.java

--- src/main/java/net/lax1dude/eaglercraft/v1_8/profile/DefaultSkins.java
+++ src/main/java/net/lax1dude/eaglercraft/v1_8/profile/DefaultSkins.java
@@ -48,7 +48,7 @@ public enum DefaultSkins {
 	WEIRD_CLIMBER_DUDE(25, "Weird Climber Dude", new ResourceLocation("eagler:mesh/weirdclimber.fallback.png"), SkinModel.WEIRD_CLIMBER_DUDE),
 	LAXATIVE_DUDE(26, "Laxative Dude", new ResourceLocation("eagler:mesh/laxativedude.fallback.png"), SkinModel.LAXATIVE_DUDE),
 BABY_CHARLES(27, "Baby Charles", new ResourceLocation("eagler:mesh/charles.fallback.png"), SkinModel.BABY_CHARLES),
-	BABY_WINSTON(27, "Baby Winston", new ResourceLocation("eagler:mesh/winston.fallback.png"), SkinModel.BABY_WINSTON);
+	BABY_WINSTON(28, "Baby Winston", new ResourceLocation("eagler:mesh/winston.fallback.png"), SkinModel.BABY_WINSTON);
 	
 	public static final DefaultSkins[] defaultSkinsMap = new DefaultSkins[29];
 	
