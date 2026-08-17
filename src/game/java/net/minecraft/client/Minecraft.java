43693ddf932579551a4c13bd225472595286240d
Joshua Rattles <363243@sps.org>
2026-08-17 12:48:06 -0500

    Update Minecraft.java

--- src/game/java/net/minecraft/client/Minecraft.java
+++ src/game/java/net/minecraft/client/Minecraft.java
@@ -18,7 +18,6 @@ import net.lax1dude.eaglercraft.v1_8.EagRuntime;
 import net.lax1dude.eaglercraft.v1_8.EagUtils;
 import net.lax1dude.eaglercraft.v1_8.HString;
 import net.lax1dude.eaglercraft.v1_8.IOUtils;
-import net.lax1dude.eaglercraft.v1_8.GuiShiftMenu;
 import net.lax1dude.eaglercraft.v1_8.Keyboard;
 import net.lax1dude.eaglercraft.v1_8.Mouse;
 import net.lax1dude.eaglercraft.v1_8.PauseMenuCustomizeState;
