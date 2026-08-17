4728964528f3bd8d3c872579b713a7c9bf80f0c9
Joshua Rattles <363243@sps.org>
2026-08-17 11:52:51 -0500

    Update GuiShiftMenu.java

--- src/main/java/net/lax1dude/eaglercraft/v1_8/GuiShiftMenu.java
+++ src/main/java/net/lax1dude/eaglercraft/v1_8/GuiShiftMenu.java
@@ -1,7 +1,6 @@
 package net.lax1dude.eaglercraft.v1_8;
 
-import net.lax1dude.eaglercraft.v1_8.minecraft.GuiScreen;
-import net.lax1dude.eaglercraft.v1_8.opengl.GlStateManager;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiButton;
 import java.io.IOException;
 
@@ -12,7 +11,6 @@ public class GuiShiftMenu extends GuiScreen {
     @Override
     public void initGui() {
         this.buttonList.clear();
-        // Button to toggle Fullbright
         this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height / 2 - 20, "Fullbright: OFF"));
     }
 
@@ -21,11 +19,10 @@ public class GuiShiftMenu extends GuiScreen {
         if (button.id == 0) {
             fullbrightEnabled = !fullbrightEnabled;
             button.displayString = "Fullbright: " + (fullbrightEnabled ? "ON" : "OFF");
-            // Apply the Fullbright effect
             if (fullbrightEnabled) {
-                mc.gameSettings.gammaSetting = 100.0f; // Enable Fullbright
+                mc.gameSettings.gammaSetting = 100.0f;
             } else {
-                mc.gameSettings.gammaSetting = 1.0f; // Reset to normal brightness
+                mc.gameSettings.gammaSetting = 1.0f;
             }
         }
     }
@@ -33,12 +30,12 @@ public class GuiShiftMenu extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.drawDefaultBackground();
-        this.drawCenteredString(this.fontRendererObj, "Shift Menu", this.width / 2, this.height / 2 - 60, 0xFFFFFF);
+        this.drawCenteredString(this.fontRendererObj, "My Custom Menu", this.width / 2, this.height / 2 - 60, 0xFFFFFF);
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
     public boolean doesGuiPauseGame() {
-        return false; // Menu does not pause the game
+        return false;
     }
 }
